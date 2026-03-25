1e566796ddaa2d5ba11eebd9142d552319729450
rohit <rohitkalgude.xitijinfo@gmail.com>
2026-03-25 14:02:26 +0530

    adding

--- socket.js
+++ socket.js
@@ -6,8 +6,8 @@ import { sendPushNotification } from "./utils/notificationService.js";
 export const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
-      // origin: "https://chat-frontend-eight-nu.vercel.app",
+      // origin: "http://localhost:5173",
+      origin: "https://chat-frontend-eight-nu.vercel.app",
       credentials: true,
     },
   });
